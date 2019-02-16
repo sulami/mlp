3ee079d1eae3a80cc1f4ab5e41d04affca3b3db9
Robin Schroer <sulami@peerwire.org>
2019-02-16 15:08:38 +0100

    Scaffold a new library

---
+++ README.md
@@ -0,0 +1,21 @@
+# mlp
+[![Build Status](https://travis-ci.org/sulami/mlp.svg?branch=master)](https://travis-ci.org/sulami/mlp)
+[![codecov](https://codecov.io/gh/sulami/mlp/branch/master/graph/badge.svg)](https://codecov.io/gh/sulami/mlp)
+[![Clojars Project](https://img.shields.io/clojars/v/mlp.svg)](https://clojars.org/mlp)
+
+A Clojure library designed to ... well, that part is up to you.
+
+```clj
+[mlp "0.0.0"]
+```
+
+## Usage
+
+FIXME
+
+## License
+
+Copyright © 2018 FIXME
+
+Distributed under the Eclipse Public License either version 1.0 or (at
+your option) any later version.
---
+++ project.clj
@@ -0,0 +1,13 @@
+(defproject mlp "0.0.1"
+  :description "Multi-Level Perceptron Library"
+  :url "https://github.com/sulami/mlp"
+  :license {:name "Eclipse Public License"
+            :url  "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies []
+  :plugins [[lein-cloverage "1.0.13"]
+            [lein-shell "0.5.0"]
+            [lein-ancient "0.6.15"]
+            [lein-changelog "0.3.2"]]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.10.0"]]}}
+  :deploy-repositories [["releases" :clojars]]
+  :aliases {"update-readme-version" ["shell" "sed" "-i" "s/\\\\[mlp \"[0-9.]*\"\\\\]/[mlp \"${:version}\"]/" "README.md"]})
